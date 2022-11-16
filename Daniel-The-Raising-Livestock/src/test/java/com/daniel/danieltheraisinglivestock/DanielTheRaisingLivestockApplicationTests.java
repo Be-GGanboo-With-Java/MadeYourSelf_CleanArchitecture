f45cc20e6806bf8a94de65c8b4980ef4094ad5b3
NamYoung Kim <095201a@naver.com>
2022-11-17 08:59:23 +0900

    feat : daniel the project init

---
+++ Daniel-The-Raising-Livestock/src/test/java/com/daniel/danieltheraisinglivestock/DanielTheRaisingLivestockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.danieltheraisinglivestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanielTheRaisingLivestockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
