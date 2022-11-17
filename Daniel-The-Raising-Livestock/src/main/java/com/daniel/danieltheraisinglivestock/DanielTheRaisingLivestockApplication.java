68addbcf119fa37a0b1966e112d76bb33f9deb64
NamYoung Kim <095201a@naver.com>
2022-11-17 09:11:37 +0900

    feat : Daniel the project init

--- Daniel-The-Raising-Livestock/src/main/java/com/daniel/danieltheraisinglivestock/DanielTheRaisingLivestockApplication.java
+++ Daniel-The-Raising-Livestock/src/main/java/com/daniel/danieltheraisinglivestock/DanielTheRaisingLivestockApplication.java
@@ -9,5 +9,4 @@ public class DanielTheRaisingLivestockApplication {
     public static void main(String[] args) {
         SpringApplication.run(DanielTheRaisingLivestockApplication.class, args);
     }
-
 }
